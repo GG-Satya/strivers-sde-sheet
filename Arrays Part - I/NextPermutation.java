b0838f89df5fabb900409baaecfdaf8e5172b319
GG-Satya <dsatyaprasanna@gmail.com>
2024-05-01 00:43:40 +0530

    Next permutation solution added

---
+++ Arrays Part - I/NextPermutation.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length, index = -1;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+        if(index == -1){
+            // last permutation
+            reverse(nums, 0, n-1);
+            return;
+        }
+        for(int i=n-1; i>index; i--){
+            if(nums[i] > nums[index]){
+                swap(nums,index,i);
+                break;
+            }
+        }
+        reverse(nums,index+1, n-1);
+    }
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    private void reverse(int[] nums, int i, int j){
+        while(i<j){
+            swap(nums, i++, j--);
+        }
+    }
+}
