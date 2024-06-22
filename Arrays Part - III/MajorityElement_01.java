445529cfd1084b34b450b7a98b075d20d9c3d8b6
GG-Satya <dsatyaprasanna@gmail.com>
2024-06-22 19:55:23 +0530

    Added effecient solutions

--- Arrays Part - III/MajorityElement_01.java
+++ Arrays Part - III/MajorityElement_01.java
@@ -1,6 +1,27 @@
 public class MajorityElement_01 {
+
+    //Approach-01
+
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
         return nums[nums.length/2];
+    }
+
+
+
+    //Approach-02 (More Effecient)
+
+    public int majorityElement(int[] nums) {
+        int count = 1, major = nums[0];
+        for(int i=1; i<nums.length; i++){
+            if(count == 0){
+                count++;
+                major = nums[i];
+            }else if(major == nums[i]){
+                count++;
+            }else{
+                count--;
+            }
+        }return major;
     }
 }
