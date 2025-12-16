767247b4a8dba4fe5abb9d882159670f046b842f
Satyaprasanna Dash <dsatyaprasanna@gmail.com>
2025-12-16 21:15:23 +0530

    Add Two Number Problem Solution added

---
+++ Day 5 - Linked List/Add_Two_Number.java
@@ -0,0 +1,35 @@
+public class Add_Two_Number{
+    /**
+   * Definition for singly-linked list.
+   * public class ListNode {
+   *     int val;
+   *     ListNode next;
+   *     ListNode() {}
+   *     ListNode(int val) { this.val = val; }
+   *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+   * }
+   */
+  
+  class Solution {
+      public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+          ListNode ans = new ListNode(0);
+          ListNode curr = ans;
+          int sum = 0;
+          while(l1 != null || l2 != null){
+              sum = sum/10;
+              if(l1 != null){
+                  sum += l1.val;
+                  l1 = l1.next;
+              }
+              if(l2 != null){
+                  sum += l2.val;
+                  l2 = l2.next;
+              }
+              curr.next = new ListNode(sum % 10);
+              curr = curr.next;
+          }
+          if(sum/10 == 1) curr.next = new ListNode(1);
+          return ans.next;
+      }
+  }
+}
