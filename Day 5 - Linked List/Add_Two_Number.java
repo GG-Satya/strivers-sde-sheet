b3e664daecbf162256f96e828ea056e29afa0f33
Satyaprasanna Dash <dsatyaprasanna@gmail.com>
2025-12-16 21:16:18 +0530

    updated class name

--- Day 5 - Linked List/Add_Two_Number.java
+++ Day 5 - Linked List/Add_Two_Number.java
@@ -1,5 +1,4 @@
-public class Add_Two_Number{
-    /**
+/**
    * Definition for singly-linked list.
    * public class ListNode {
    *     int val;
@@ -10,26 +9,25 @@ public class Add_Two_Number{
    * }
    */
   
-  class Solution {
-      public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-          ListNode ans = new ListNode(0);
-          ListNode curr = ans;
-          int sum = 0;
-          while(l1 != null || l2 != null){
-              sum = sum/10;
-              if(l1 != null){
-                  sum += l1.val;
-                  l1 = l1.next;
-              }
-              if(l2 != null){
-                  sum += l2.val;
-                  l2 = l2.next;
-              }
-              curr.next = new ListNode(sum % 10);
-              curr = curr.next;
+class Solution {
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+      ListNode ans = new ListNode(0);
+      ListNode curr = ans;
+      int sum = 0;
+      while(l1 != null || l2 != null){
+          sum = sum/10;
+          if(l1 != null){
+              sum += l1.val;
+              l1 = l1.next;
           }
-          if(sum/10 == 1) curr.next = new ListNode(1);
-          return ans.next;
+          if(l2 != null){
+              sum += l2.val;
+              l2 = l2.next;
+          }
+          curr.next = new ListNode(sum % 10);
+          curr = curr.next;
       }
+      if(sum/10 == 1) curr.next = new ListNode(1);
+      return ans.next;
   }
 }
